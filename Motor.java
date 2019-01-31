ebbcdf066704e6966fe03ea140ba7ff2220f19c8
danaweaver <danaweaver@users.noreply.github.com>
2019-01-31 10:36:40 -0500

    Update Motor.java

--- Motor.java
+++ Motor.java
@@ -2,28 +2,28 @@
  * Elevator Motor
  */
 public class Motor {
-	private boolean isOn;
+	private int state; //0 for stop, 1 for up, 2 for down
 	
 	public Motor()
 	{
-		isOn = false;
+		state = 0;
 	}
 	
 	/**
 	 * Get the state of the elevator motor.
-	 * @return True is motor is on, otherwise will return false
+	 * @return 0 if motor is stopped, 1 if motor is going up, 2 if motor is going down
 	 */
-	public boolean getMotorState()
+	public int getMotorState()
 	{
-		return isOn;
+		return state;
 	}
 	
 	/**
 	 * Set elevator motor state.
-	 * @param state True for on, false for off
+	 * @param state 0 to stop motor, 1 to go up, 2 to go down
 	 */
-	public void setMotorState(boolean state)
+	public void setMotorState(int state)
 	{
-		isOn = state;
+		this.state = state;
 	}
 }
